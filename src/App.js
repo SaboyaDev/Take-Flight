bd3ebf8c4aab265d8d96b64e7e0c76db4c881aa7
rsteinkopfjr <rob.steinkopf@gmail.com>
2019-04-23 19:05:10 -0400

    Add basic controller

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import DroneState from './components/DroneState';
+import Commands from './components/Commands';
+
+import styled, { createGlobalStyle } from 'styled-components';
 
 import './App.css';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: white;
+    /* font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; */
+    font-family: 'Operator Mono', monospace;
+    font-weight: 900;
+    font-size: 1rem;
+    background:#193549;
+    color: white;
+  }
+  * {
+    font-family: 'Operator Mono', monospace;
+    box-sizing: border-box;
+  }
+  h2 {
+    text-align: center;
+    font-style: italic;
+  }
+`;
+
+const PageStyles = styled.div`
+  max-width: 500px;
+  margin: 0 auto;
+`;
+
 class App extends Component {
   render() {
-    return <h1>Dronny-Bot</h1>;
+    return <PageStyles>
+      <h2>JavaScript Drone</h2>
+      <GlobalStyle />
+      <Commands />
+      <DroneState />
+    </PageStyles>;
   }
 }
 
