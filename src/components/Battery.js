d3ca68e2dcc2c3e6a5cb51df194c9a5056390e9e
rsteinkopfjr <rob.steinkopf@gmail.com>
2019-04-27 10:37:13 -0400

    Remove commands css

--- src/components/Battery.js
+++ src/components/Battery.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import React, { Component } from 'react';
+import React from 'react';
 
 const BatteryStyles = styled.div`
   width: 100%;
--- src/components/Commands.js
+++ src/components/Commands.js
@@ -1,71 +1,5 @@
-import styled from 'styled-components';
 import socket from '../socket';
-import React, { Component } from 'react';
-
-const CommandGrid = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1.25fr 1fr;
-  grid-template-rows: repeat(3, 1fr);
-  border: 1px solid black;
-  grid-gap: 3px;
-  button {
-    text-shadow: 2px 2px 0 rgba(0, 0, 0, 0.05);
-    border: 0;
-    background: #fe2c70;
-    border: 4px solid transparent;
-    color: white;
-    font-size: 1rem;
-    position: relative;
-    &:active {
-      top: 2px;
-    }
-    &:focus {
-      outline: 0;
-      border-color: #ffc600;
-    }
-    &.takeoff {
-      background: #41c7ff;
-    }
-    &.land {
-      background: #00ff00;
-    }
-    &.emergency {
-      background: orange;
-      text-transform: uppercase;
-      color: black;
-    }
-    &.rotate {
-      background: #00fff9;
-      color: black;
-    }
-    &.height {
-      background: #fff;
-      color: black;
-    }
-    span.symbol {
-      display: block;
-      font-size: 2rem;
-      font-weight: 400;
-    }
-  }
-  .center {
-    display: grid;
-    grid-gap: 3px;
-    grid-template-columns: 1fr 1fr;
-    button:last-child {
-      grid-column: span 2;
-    }
-  }
-  h2 {
-    grid-column: 1 / -1;
-    background: #ffc600;
-    margin: 0;
-    font-size: 1rem;
-    text-align: center;
-    padding: 0.5rem;
-    color: black;
-  }
-`;
+import React from 'react';
 
 function sendCommand(command) {
     return function () {
@@ -76,7 +10,7 @@ function sendCommand(command) {
 
 const amount = 100;
 const Commands = () => (
-    <CommandGrid>
+    <div>
         <button className="rotate" onClick={sendCommand('ccw 90')}>
             <span className="symbol">⟲</span> 90°
     </button>
@@ -122,7 +56,7 @@ const Commands = () => (
         <button onClick={sendCommand('curve 100 100 100 150 250 350 50')}>
             Curve!
     </button>
-    </CommandGrid>
+    </div>
 );
 
 export default Commands;
