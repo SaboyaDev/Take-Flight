e55436f8cf7280d8b0eda5d530bf2fcc4306e87b
Jose L Saboya Rosales <saboya@Joses-MBP.home>
2019-05-02 13:58:32 -0400

    chnage the port for the httpserver

--- back-end/fly-drone.js
+++ back-end/fly-drone.js
@@ -9,7 +9,7 @@ const port_cmd = 8889; // Tello Main Port
 const port_status = 8890; // Tello Status Port
 const port_video = 11111; // Tello Video Port
 const port_websocket = 8080; // Websocket Port
-const port_httpServer = 3000; // Express HTTP Server Port
+const port_httpServer = 3001; // Express HTTP Server Port
 
 const HOST = '192.168.10.1';
 
